82931b52ebc5a07aef9fdb235d04de8f34fb9729
natamaz <sidorova87@mail.ru>
2025-07-08 17:40:43 +0200

    initial commit

---
+++ src/js/data.js
@@ -0,0 +1,99 @@
+export const projects = [
+  {
+    title: 'Habitat',
+    image: '/img-1.png',
+    year: 2022,
+    href: 'https://natamaz.github.io/Habitat/',
+  },
+  {
+    title: 'Plemya-lend',
+    image: '/img-25.png',
+    year: 2019,
+    href: 'https://natamaz.github.io/Plemya-lend/',
+  },
+  {
+    title: 'heyLoc',
+    image: '/img-5.png',
+    year: 2023,
+    href: 'https://natamaz.github.io/heyLoc/',
+  },
+  {
+    title: 'Plemja-home',
+    image: '/img-29.png',
+    year: 2019,
+    href: 'https://natamaz.github.io/Plemja-home/',
+  },
+  {
+    title: 'Dr.R',
+    image: '/img-2.png',
+    year: 2023,
+    href: 'https://natamaz.github.io/Dr.R/',
+  },
+  {
+    title: 'Bigartshop',
+    image: '/img-12.png',
+    year: 2017,
+    href: 'https://natamaz.github.io/Bigartshop/',
+  },
+  {
+    title: 'AdeXX',
+    image: '/img-9.png',
+    year: 2019,
+    href: 'https://natamaz.github.io/AdeXX/',
+  },
+  {
+    title: 'Stylelook',
+    image: '/img-7.png',
+    year: 2020,
+    href: 'https://natamaz.github.io/Stylelook/',
+  },
+  {
+    title: 'AFT-promo',
+    image: '/img-10.png',
+    year: 2018,
+    href: 'https://natamaz.github.io/AFT-promo/',
+  },
+  {
+    title: 'Cripta',
+    image: '/img-4.png',
+    year: 2021,
+    href: 'https://natamaz.github.io/Cripta/',
+  },
+  {
+    title: 'jiu-jitsu',
+    image: '/img-3.png',
+    year: 2023,
+    href: 'https://natamaz.github.io/jiu-jitsu/',
+  },
+  {
+    title: 'Injir',
+    image: '/img-21.png',
+    year: 2019,
+    href: 'https://natamaz.github.io/Injir/',
+  },
+  {
+    title: 'Burgers',
+    image: '/img-22.png',
+    year: 2019,
+    href: 'https://natamaz.github.io/Burgers/',
+  },
+  {
+    title: 'Barabulja',
+    image: '/img-23.png',
+    year: 2019,
+    href: 'https://natamaz.github.io/Barabulja/',
+  },
+  {
+    title: 'Consalting',
+    image: '/img-24.png',
+    year: 2018,
+    href: 'https://natamaz.github.io/Consalting/',
+  },
+  {
+    title: 'Cadaster',
+    image: '/img-13.png',
+    year: 2018,
+    href: 'https://natamaz.github.io/Cadaster1/',
+  },
+
+]
---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+
+export default defineConfig({
+  // base:'/',
+  base:'/MyProgect/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+})
